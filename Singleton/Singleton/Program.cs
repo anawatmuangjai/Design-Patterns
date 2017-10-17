605a7a1aeece4731ae7e0d699d516c37115815c7
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-17 12:28:53 +0700

    Add singleton example

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,53 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }
+
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> _capitals;
+
+        public SingletonDatabase()
+        {
+            Console.WriteLine("Initailizing database");
+
+            _capitals = File.ReadAllLines("Capitals.txt")
+                .Batch(2)
+                .ToDictionary
+                (
+                    list => list.ElementAt(0).Trim(),
+                    list => int.Parse(list.ElementAt(1))
+                );
+        }
+
+        public int GetPopulation(string name)
+        {
+            return _capitals[name];
+        }
+
+        private static Lazy<SingletonDatabase> instance = 
+            new Lazy<SingletonDatabase>(() => new SingletonDatabase());
+
+        public static SingletonDatabase Instance => instance.Value;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDatabase.Instance;
+            var city = "Tokyo";
+            Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
+        }
+    }
+}
